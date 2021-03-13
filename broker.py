d241b4b11b72baf741d78aa52b2cc485cc4332cb
edmasters <76230161+edmasters@users.noreply.github.com>
2021-03-13 15:03:28 -0800

    zookeeper broker

---
+++ broker.py
@@ -0,0 +1,115 @@
+
+import zmq
+import os
+import sys
+import time
+import threading
+import zmq
+from random import randrange
+
+from kazoo.client import KazooClient
+from kazoo.client import KazooState
+import logging
+
+logging.basicConfig()
+
+class broker:
+
+	def __init__(self):
+		#setting up proxy
+		self.context = zmq.Context()
+		self.frontend = self.context.socket(zmq.XPUB)
+		self.backend = self.context.socket(zmq.XSUB)
+
+		#zookeeper nodes
+		self.zk_object = KazooClient(hosts='127.0.0.1:2181')
+		self.zk_object.start()
+		self.path = '/home'
+
+		#znode for each broker
+		node1 = self.path + "broker1"
+		if self.zk_object.exists(node1):
+			pass
+		else:
+			#make sure the path exists
+			self.zk_object.ensure_path(self.path)
+			#create the znode with port info for the pubs + subs to use in addr[]
+			self.zk_object.create(node1, "5555,5556")
+
+		#znode2 (same w/ modified port #'s')
+		node2 = self.path + "broker2"
+		if self.zk_object.exists(node2):
+			pass
+		else:
+			#make sure the path exists
+			self.zk_object.ensure_path(self.path)
+			#create the znode with port info for the pubs + subs to use in addr[]
+			self.zk_object.create(node1, "5557,5558")
+
+		#znode 3
+		node3 = self.path + "broker3"
+		if self.zk_object.exists(node2):
+			pass
+		else:
+			#make sure the path exists
+			self.zk_object.ensure_path(self.path)
+			#create the znode with port info for the pubs + subs to use in addr[]
+			self.zk_object.create(node1, "5559,5560")
+
+		#leader election among brokers (1st time)
+		self.election = self.zk_object.Election(self.path, "leader")
+		potential_leaders = self.election.contenders()
+		self.leader = potential_leaders[0].encode('latin-1')
+		print("Leader ports: " + self.leader)
+
+		#use port #'s from the leader to finish connecting the proxy'
+		addr = self.leader.split(",")
+		self.frontend.bind("tcp://*:" + addr[0])
+		self.backend.bind("tcp://*:" + addr[1])
+
+		#leader node creation + set-up
+		self.watch_dir = self.path + self.leader
+		self.leader_path ="/leader/"
+		self.leader_node = self.leader_path + "node"
+		if self.zk_object.exists(self.leader_node):
+			pass
+		#make if it doesn't exists
+		else:
+			self.zk_object.ensure_path(self.leader_path)
+			self.zk_object.create(self.leader_node, ephemeral = True)
+
+		#setting
+		self.zk_object.set(self.leader_node, self.leader)
+
+	def device(self):
+		#start the proxy
+		zmq.device(zmq.FORWARDER, self.frontend, self.backend)
+
+
+	def monitor(self):
+		while True:
+			@self.zk_object.DataWatch(self.watch_dir)
+			def watch_node(data, stat, event):
+				#url's for unbinding later
+				addr = self.leader.split(",")
+				front_url = "tcp://*:" + addr[0]
+				back_url = "tcp://*:" + addr[1]
+				#re-elect leader if node is gone
+				if event.type == "DELETED":
+					self.election = self.zk_object.Election(self.path, "leader")
+					potential_leaders = self.election.contenders()
+					self.leader = potential_leaders[0].encode('latin-1')
+					#reset node
+					self.zk_object.set(self.leader_node, self.leader)
+
+					#unbind + re-bind broker ports
+					self.frontend.unbind(front_url)
+					self.backend.unbind(back_url)
+					self.frontend.bind("tcp://*:" + addr[0])
+					self.backend.bind("tcp://*:" + addr[1])
+
+			election.run(self.device)
+
+if __name__ == "__main__":
+    broker = broker()
+    broker.monitor()
