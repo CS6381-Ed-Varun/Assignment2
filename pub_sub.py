9f09312c946d179c582ec67f1c3eb2e6798eac95
edmasters <76230161+edmasters@users.noreply.github.com>
2021-03-16 13:50:41 -0700

    znode for listener

--- pub_sub.py
+++ pub_sub.py
@@ -71,9 +71,9 @@ class subscriber(Thread):
 			topic, messagedata = string.split()
 			print (topic, messagedata)
 			
-	#for leaving - same as assignment1
+	#for leaving 
 	def close(self):
-		self.sub.close(0)
+		self.joined = False
 		print('sub ' + self.topic + ' leaving')
 
 class publisher(Thread):
@@ -142,18 +142,21 @@ class publisher(Thread):
 			time.sleep(1)
 
 	def close(self):
-		self.pub.close()
+		self.joined = False
 		print('pub leaving')
 
 class listener(Thread):
-	
-	# ED NEEDS TO ADD ZooKeeper functionality here !!!! Not ready
 	
 	#init self
 	def __init__(self, flood):
 		super().__init__()
 		self.flood = flood
 		self.joined = True
+		self.broker = broker_add
+		self.path = '/leader/node'
+		#port from zk config file, host ip should be changed
+		self.zk_object = KazooClient(hosts='127.0.0.1:2181')
+		self.zk_object.start()
 
 	#start up the thread
 	def run(self):
@@ -168,15 +171,44 @@ class listener(Thread):
 				sub.setsockopt_string(zmq.SUBSCRIBE, "")
 		#Broker version - connect w/o filtering
 		else:
-			sub.connect("tcp://127.0.0.1:5559")
+			data, stat = self.zk_object.get(self.path) #get port #'s from the leader's zk node
+			data = str(data) 
+			addr = data.split(",")  #type casting since path is bytes and strings are needed for connect
+			addr[0] = addr[0][2:]   #removing a ' from the byte -> string cast
+			print("tcp://" + self.broker + ":" + addr[0])	
+			self.sub.connect("tcp://" + self.broker + ":" + addr[0]) #connecting to the broker
 			sub.setsockopt_string(zmq.SUBSCRIBE, "")
-		#make a list of messages and appended to it each time one arrives
+		
+		#make a list of messages and appended to it
 		messages = []
 		while self.joined:
+			#setting our zk watch on the broker node
+			@self.zk_object.DataWatch(self.path)
+			def watch_node(data, stat, event): 
+				#required to allow us to check the type lower down
+				if event != None:	
+					#if anything happens to the znode: close the connections, sleep, and then reconnect using new leader ports
+					if event.type == "CHANGED":
+						self.sub.close()
+						self.context.term()
+						time.sleep(2)
+						self.context = zmq.Context()
+						self.sub = self.context.socket(zmq.SUB)
+						#reconnect to broker
+						data, stat = self.zk_object.get(self.path)
+						data = str(data)
+						addr = data.split(",") #same type casting as above for bytes -> string
+						addr[0] = addr[0][2:]
+						self.sub.connect("tcp://" + self.broker + ":" + addr[0])
+						
 			string = sub.recv()
 			messages.append(messages)
 			if (len(messages) % 10 == 0):
 				print(str(len(messages)) + " messages sent by brokers")
+	def close(self):
+		self.joined = False
+		print('listener leaving')
+		
 			
 #initializing the individual pubs, sub, and listener
 def main():
